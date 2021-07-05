21cae12e8376fdff155504b5d286236a84c42bf1
krishangpatney <krishangpatney@icloud.com>
2021-07-06 03:35:08 +0400

    added cors fix

--- backend/settings.py
+++ backend/settings.py
@@ -28,10 +28,6 @@ ALLOWED_HOSTS = ['locus-prod-backend.herokuapp.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-    'https://locus-prod-backend.herokuapp.com',
-)
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
