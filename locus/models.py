7f1bb691d6ee66bf9551bebf04f8650fbc6bdd88
krishangpatney <krishangpatney@icloud.com>
2021-07-06 14:41:07 +0400

    API models - Work in theory

--- locus/models.py
+++ locus/models.py
@@ -7,7 +7,7 @@ class Geohash(models.Model):
 class Track(models.Model):
     time_added = models.DateTimeField(auto_now_add=True)
     expiry_time = models.DateTimeField(default = datetime.now() + timedelta(hours=10))
-
+    geohash_link = models.ForeignKey(Geohash, on_delete=models.CASCADE, default=None)
     track_title = models.CharField(max_length=100, default='')
     track_author = models.CharField(max_length=100, default='')
     track_url = models.URLField(default='')
