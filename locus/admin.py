10d2aba9a013514d7c04493f4a782516ebd85631
krishangpatney <krishangpatney@icloud.com>
2021-07-06 14:29:30 +0400

    added API models

--- locus/admin.py
+++ locus/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from locus.models import Geohash, Track
 # Register your models here.
+admin.site.register(Geohash)
+admin.site.register(Track)
--- locus/models.py
+++ locus/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from datetime import datetime, timedelta
 # Create your models here.
+class Geohash(models.Model):
+    hash_code = models.CharField(max_length=8 ,default='')
+
+class Track(models.Model):
+    time_added = models.DateTimeField(auto_now_add=True)
+    expiry_time = models.DateTimeField(default = datetime.now() + timedelta(hours=10))
+
+    track_title = models.CharField(max_length=100, default='')
+    track_author = models.CharField(max_length=100, default='')
+    track_url = models.URLField(default='')
+
+    # This should be the queue
+    class Meta:
+        ordering = ('time_added', )
